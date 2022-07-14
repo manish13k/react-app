d79ab7d0a90306e307e1567687b8fff2d2c0a76d
Manish Sangwan <manish.sangwan@autodatacorp.org>
2022-07-14 10:26:58 +0530

    Implemented Album and Photo API with Modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import AlbumList from "./components/album";
+import Photo from "./components/photo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+            <div data-testid="main-app" className="App">
+                <Router>
+                    <Routes>
+                        <Route exact path="/" element={<AlbumList/>}/>
+                        <Route exact path="/photo" element={<Photo/>}/>
+                    </Routes>
+                </Router>
+            </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const divElement = screen.getByTestId('main-app');
+  expect(divElement).toBeInTheDocument();
 });
